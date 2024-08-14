ce81a346bfe4883335a1d2c0d48b32f041257375
Sandra Karime Gutiérrez Martínez <sandra.gutierrez@logrand.local>
2024-08-13 20:00:47 -0600

    questions functions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,42 @@
 // TODO: Include packages needed for this application
-
+const generateMarkdown = requiere(".\utils\generateMarkdown.js");
+const inquirer= require ("inquirer");
+const fs =require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+{
+    type:"input",
+    message: "What's your GitHub username?",
+    name:"username"
+},
+{
+    type:"input",
+    message: "What's your repository name?",
+    name:"repo"
+},
+{
+    type:"input",
+    message: "What's your project title?",
+    name:"title"
+},
+{
+    type:"input",
+    message: "What's your repository name?",
+    name:"description"
+},
+{
+    type:"input",
+    message: "Describe how to instal your app:",
+    name:"installation"
+},
+{
+    type:"input",
+    message: "Explain how to use your app:",
+    name:"usage"
+},
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
