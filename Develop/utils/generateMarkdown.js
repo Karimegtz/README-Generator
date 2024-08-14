3461aa2315dbcf8e3a792f0a5a8be16d92bfdd9c
Sandra Karime Gutiérrez Martínez <sandra.gutierrez@logrand.local>
2024-08-13 19:44:10 -0600

    function creation

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,21 +1,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
+function renderLicenseBadge(data) {
 
-
+return `![GitHub](https://img.shields.io/github/license/${data.username}/${data.repo}?style=for-the-badge)`
 
 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+
+  return `https://github.com/${data.username}/${data.repo}/blob/main/LICENSE` 
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(data) {
+
+return  ` ${renderLicenseBadge(data)}<br>
+this project is under a license (${renderLicenseLink(data)}).See the LICENCE file for more details.
+`
+
+}
 
 // TODO: Create a function to generate markdown for README
+//estructura del readme
 function generateMarkdown(data) {
   return `# ${data.title}
 
@@ -37,47 +47,18 @@ ${data.installation}
 
 ## Usage
 
-Go to https://symmetrical-couscous-1.onrender.com/ and sign up with a username, email and password. <br><br>
-
-Enter the type of money transaction along with the category it belongs to. <br><br>
-
-Pie chart will be updated with user's transactions input. This chart will help the user visualize how much money is coming in and how much money is going out. <br><br>
-
+${data.usage}
 
 
 ## Credits
 
-Carlos Jorge Gonzalez https://github.com/steakbutter <br>
-Cristian Loera https://github.com/el-bicho7 <br>
-Dalia  https://github.com/dali-gogo <br>
-Karime Gutierrez https://github.com/Karimegtz
+Karime Gutierrez https://github.com/${data.username}
 
 ## License
-
-MIT License
-
-Copyright (c) 2024 Carlos
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
+${renderLicenseSection(data)}
 
 `;
+
 }
 
 module.exports = generateMarkdown;
